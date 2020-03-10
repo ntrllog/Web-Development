c828c3f0d331a1b56037cde2822e3bf91e065329
alan-lam <alanlam00@gmail.com>
2020-03-09 22:07:50 -0700

    update authentication

--- Authentication/app.js
+++ Authentication/app.js
@@ -31,7 +31,8 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  secret: String
 });
 
 // userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
@@ -57,12 +58,43 @@ app.get('/register', function(req, res) {
 });
 
 app.get('/secrets', function(req, res) {
+  User.find({'secret': {$ne: null}}, function(err, foundUsers) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      if (foundUsers) {
+        res.render('secrets', {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get('/submit', function(req, res) {
   if (req.isAuthenticated()) {
-    res.render('secrets');
+    res.render('submit');
   }
   else {
     res.redirect('/login');
   }
+});
+
+app.post('/submit', function(req, res) {
+  const submittedSecret = req.body.secret;
+  
+  User.findById(req.user.id, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function() {
+          res.redirect('/secrets');
+        });
+      }
+    }
+  });
 });
 
 app.get('/logout', function(req, res) {
